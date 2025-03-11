bc6276b57c0e2713fecca07a546a10cb4eb8d35d
hank.lin <hank040@jcg-tech.com>
2025-03-11 13:15:03 +0800

    feat: add metadata for seo

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,20 @@
-export default function page() {
+import { Metadata } from "next";
+import Form from 'next/form'
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default function Page() {
   return (
+    <div>
     <p>Customers Page</p>
+    <Form action="/dashboard/customers">
+      {/* On submission, the input value will be appended to
+          the URL, e.g. /search?query=abc */}
+      <input name="query" />
+      <button type="submit">Submit</button>
+    </Form>
+    </div>
   );
 };
